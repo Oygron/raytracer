0e47dddb97a3b69223bdd890b74ce328024db022
Pascal Bovo <pascal.bovo@isae-supaero.fr>
2024-09-10 17:56:49 +0200

    tests renderer en cours

--- src/scene/renderer.rs
+++ src/scene/renderer.rs
@@ -22,10 +22,6 @@ const NB_ITER: usize = 100;
 const NB_WORKERS: usize = 4;
 
 impl Scene{
-    pub fn toto(&self) -> u64 {
-        0
-    }
-
     pub fn render_rayon(&self) -> Vec<f64> {
 
         let mut data: Vec<f64> =
@@ -46,7 +42,7 @@ impl Scene{
             }
         }
 
-        normalize(data)
+        Self::normalize(data)
     }
 
     pub fn render_multithread(&self) -> Vec<f64> {
@@ -73,7 +69,7 @@ impl Scene{
             }
         }
     
-        normalize(data)
+        Self::normalize(data)
     }
     
     pub fn render_monothread(&self) -> Vec<f64> {
@@ -92,7 +88,7 @@ impl Scene{
             }
         }
     
-        normalize(data)
+        Self::normalize(data)
     }
 
     fn render_pixel(&self, l: u32, c: u32) -> Color {
@@ -102,169 +98,170 @@ impl Scene{
     
         let ray = self.camera.ray((c, l));
     
-        send_ray(self, ray, MAX_BOUNCES)
+        self.send_ray(ray, MAX_BOUNCES)
     }
 
-}
-
-
-fn compute_diffuse(scene: &Scene, i: &Intersect) -> Color{
+    fn compute_diffuse(&self, i: &Intersect) -> Color{
     
-    let mut color =
-        i.material.diffuse * scene.ambiant_light.color * scene.ambiant_light.intensity;
-
-    for light in scene.lights.iter() {
-        if let LightType::PointLight { pos: light_pos } = light.light_type {
-            let light_dir = (i.pos - light_pos).normalize().unwrap();
-            let light_ray = Ray{start: light_pos, dir: light_dir};
-            let light_intersect = get_intersect(scene, light_ray);
-            let dist_light = (light_pos - i.pos).norm();
-
-            //Intersect sooner
-            if let Some(Intersect{pos:_, dist: d, normal:_, material:_}) = light_intersect {
-                if d < dist_light - 1e-9 {
+        let mut color =
+            i.material.diffuse * self.ambiant_light.color * self.ambiant_light.intensity;
+    
+        for light in self.lights.iter() {
+            if let LightType::PointLight { pos: light_pos } = light.light_type {
+                let light_dir = (i.pos - light_pos).normalize().unwrap();
+                let light_ray = Ray{start: light_pos, dir: light_dir};
+                let light_intersect = self.get_intersect(light_ray);
+                let dist_light = (light_pos - i.pos).norm();
+    
+                //Intersect sooner
+                if let Some(Intersect{pos:_, dist: d, normal:_, material:_}) = light_intersect {
+                    if d < dist_light - 1e-9 {
+                        continue;
+                    }
+                } else {
+                    //No intersect (should not happen except rounding error)
                     continue;
                 }
-            } else {
-                //No intersect (should not happen except rounding error)
-                continue;
-            }
-            
-            let factor = i.normal.dot(light_pos - i.pos);
-            if factor > 0. {
-                color = color
-                    + i.material.diffuse * light.color * light.intensity * (factor/(dist_light*dist_light));
+                
+                let factor = i.normal.dot(light_pos - i.pos);
+                if factor > 0. {
+                    color = color
+                        + i.material.diffuse * light.color * light.intensity * (factor/(dist_light*dist_light));
+                }
             }
         }
+        color
     }
-    color
-}
-
-
-fn compute_reflection(scene: &Scene, ray: Ray, i: &Intersect, depth: u16) -> Color{
     
     
-    let r = rand::random::<f64>() * i.material.roughness * PI / 2.;
-    let alpha = rand::random::<f64>() * 2. * PI;
-
-    let dir = ray.dir.symmetry(i.normal) * -1.;
-
-    //first orthogonal
-    let norm1: Vec3d;
-    //x le plus petit
-    if dir.x.abs() < dir.y.abs() &&
-       dir.x.abs() < dir.z.abs(){
-        norm1 = Vec3d{x:0., y:-dir.z, z:dir.y}.normalize().unwrap();
-    } else if dir.y.abs() < dir.z.abs(){//y le plus petit
-        norm1 = Vec3d{x:-dir.z, y:0., z:dir.x}.normalize().unwrap();
-    } else {//z le plus petit
-        norm1 = Vec3d{x:-dir.y, y:dir.x, z:0.}.normalize().unwrap();
+    fn compute_reflection(&self, ray: Ray, i: &Intersect, depth: u16) -> Color{
+        
+        
+        let r = rand::random::<f64>() * i.material.roughness * PI / 2.;
+        let alpha = rand::random::<f64>() * 2. * PI;
+    
+        let dir = ray.dir.symmetry(i.normal) * -1.;
+    
+        //first orthogonal
+        let norm1: Vec3d;
+        //x le plus petit
+        if dir.x.abs() < dir.y.abs() &&
+           dir.x.abs() < dir.z.abs(){
+            norm1 = Vec3d{x:0., y:-dir.z, z:dir.y}.normalize().unwrap();
+        } else if dir.y.abs() < dir.z.abs(){//y le plus petit
+            norm1 = Vec3d{x:-dir.z, y:0., z:dir.x}.normalize().unwrap();
+        } else {//z le plus petit
+            norm1 = Vec3d{x:-dir.y, y:dir.x, z:0.}.normalize().unwrap();
+        }
+    
+        let norm2 = dir.cross(norm1);
+    
+        let norm = alpha.cos()*norm1 + alpha.sin()*norm2;
+    
+        let dir = r.cos()*dir + r.sin()*norm;
+    
+        let symmetric_ray = Ray{start:i.pos, dir};
+    
+        self.send_ray(symmetric_ray, depth-1)*i.material.specular
     }
-
-    let norm2 = dir.cross(norm1);
-
-    let norm = alpha.cos()*norm1 + alpha.sin()*norm2;
-
-    let dir = r.cos()*dir + r.sin()*norm;
-
-    let symmetric_ray = Ray{start:i.pos, dir};
-
-    send_ray(scene, symmetric_ray, depth-1)*i.material.specular
-}
-
-fn compute_specular(scene: &Scene, ray: Ray, i: &Intersect) -> Color{
-    let symmetric_ray = Ray{start:i.pos, dir:ray.dir.symmetry(i.normal) * -1.};
-    //let mut color = Color{r:0., g:0., b:0.};
-    let mut color = i.material.specular * scene.ambiant_light.color * scene.ambiant_light.intensity;
-
-
-    for light in scene.lights.iter() {
-        if let LightType::PointLight { pos: light_pos } = light.light_type {
-            let light_dir = (i.pos - light_pos).normalize().unwrap();
-            let light_ray = Ray{start: light_pos, dir: light_dir};
-            let light_intersect = get_intersect(scene, light_ray);
-            let dist_light = (light_pos - i.pos).norm();
-
-            //Intersect sooner
-            if let Some(Intersect{pos:_, dist: d, normal:_, material:_}) = light_intersect {
-                if d < dist_light - 1e-9 {
+    
+    fn compute_specular(&self, ray: Ray, i: &Intersect) -> Color{
+        let symmetric_ray = Ray{start:i.pos, dir:ray.dir.symmetry(i.normal) * -1.};
+        //let mut color = Color{r:0., g:0., b:0.};
+        let mut color = i.material.specular * self.ambiant_light.color * self.ambiant_light.intensity;
+    
+    
+        for light in self.lights.iter() {
+            if let LightType::PointLight { pos: light_pos } = light.light_type {
+                let light_dir = (i.pos - light_pos).normalize().unwrap();
+                let light_ray = Ray{start: light_pos, dir: light_dir};
+                let light_intersect = self.get_intersect(light_ray);
+                let dist_light = (light_pos - i.pos).norm();
+    
+                //Intersect sooner
+                if let Some(Intersect{pos:_, dist: d, normal:_, material:_}) = light_intersect {
+                    if d < dist_light - 1e-9 {
+                        continue;
+                    }
+                } else {
+                    //No intersect (should not happen except rounding error)
                     continue;
                 }
-            } else {
-                //No intersect (should not happen except rounding error)
-                continue;
-            }
-
-            let factor = symmetric_ray.dir.dot(light_dir * -1.);
-            if factor > 0. {
-                let angle = factor.acos();
-                let angle = angle/i.material.roughness;
-                if angle > PI/2. {
-                    continue;
+    
+                let factor = symmetric_ray.dir.dot(light_dir * -1.);
+                if factor > 0. {
+                    let angle = factor.acos();
+                    let angle = angle/i.material.roughness;
+                    if angle > PI/2. {
+                        continue;
+                    }
+                    let factor = angle.cos(); 
+                    color = color
+                        + i.material.specular * light.color * light.intensity * (factor/(dist_light*dist_light*i.material.roughness));
                 }
-                let factor = angle.cos(); 
-                color = color
-                    + i.material.specular * light.color * light.intensity * (factor/(dist_light*dist_light*i.material.roughness));
             }
         }
+        color
     }
-    color
-}
-
-fn get_intersect(scene: &Scene, ray: Ray) -> Option<Intersect>{
-    let mut ray_intersect : Option<Intersect> = None;
-    let mut intersect_dist: f64 = f64::INFINITY;
-
-    for object in scene.objects.iter() {
-        match object {
-            Object::Sphere(s) => match s.intersect(&ray) {
-                Some(i) if i.dist < intersect_dist => {
-                    intersect_dist = i.dist;
-                    ray_intersect = Some(i);
-                }
-                _ => (),
-            },
-            Object::Rasterized(r) => match r.intersect(&ray){
-                Some(i) if i.dist < intersect_dist => {
-                    intersect_dist = i.dist;
-                    ray_intersect = Some(i);
-                }
-                _ => (),
-            },
-        }
-    }
-
-    ray_intersect
-}
-
-fn send_ray(scene: &Scene, ray: Ray, depth: u16) -> Color {
-    let mut color: Color = scene.ambiant_light.color * scene.ambiant_light.intensity;
-    if depth == 0 {
-        return color;
-    }
-
-    let ray_intersect = get_intersect(scene, ray);
-
-    match ray_intersect {
-        Some(i)=>{
-            color = compute_diffuse(scene, &i) * (1. - i.material.reflectivity);
-            if i.material.reflectivity > 0. {
-                color = color+compute_reflection(scene, ray, &i, depth) * i.material.reflectivity;
-                color = color+compute_specular(scene, ray, &i) * i.material.reflectivity;
+    
+    fn get_intersect(&self, ray: Ray) -> Option<Intersect>{
+        let mut ray_intersect : Option<Intersect> = None;
+        let mut intersect_dist: f64 = f64::INFINITY;
+    
+        for object in self.objects.iter() {
+            match object {
+                Object::Sphere(s) => match s.intersect(&ray) {
+                    Some(i) if i.dist < intersect_dist => {
+                        intersect_dist = i.dist;
+                        ray_intersect = Some(i);
+                    }
+                    _ => (),
+                },
+                Object::Rasterized(r) => match r.intersect(&ray){
+                    Some(i) if i.dist < intersect_dist => {
+                        intersect_dist = i.dist;
+                        ray_intersect = Some(i);
+                    }
+                    _ => (),
+                },
             }
-        }, 
-        _ => (),
+        }
+    
+        ray_intersect
+    }
+    
+    fn send_ray(&self, ray: Ray, depth: u16) -> Color {
+        let mut color: Color = self.ambiant_light.color * self.ambiant_light.intensity;
+        if depth == 0 {
+            return color;
+        }
+    
+        let ray_intersect = self.get_intersect(ray);
+    
+        match ray_intersect {
+            Some(i)=>{
+                color = self.compute_diffuse(&i) * (1. - i.material.reflectivity);
+                if i.material.reflectivity > 0. {
+                    color = color + self.compute_reflection(ray, &i, depth) * i.material.reflectivity;
+                    color = color + self.compute_specular(ray, &i) * i.material.reflectivity;
+                }
+            }, 
+            _ => (),
+        }
+    
+        color
+    
+    }
+    
+    
+    fn normalize(data: Vec<f64>) -> Vec<f64> {
+        let max_intensity = data.iter().cloned().fold(f64::NAN, f64::max);
+        data.iter().map(|v| (*v / max_intensity)).collect()
     }
 
-    color
-
 }
 
 
-fn normalize(data: Vec<f64>) -> Vec<f64> {
-    let max_intensity = data.iter().cloned().fold(f64::NAN, f64::max);
-    data.iter().map(|v| (*v / max_intensity)).collect()
-}
 
 #[cfg(test)]
 mod tests {
@@ -275,16 +272,12 @@ mod tests {
     #[test]
     fn test_normalize() {
         let vec = vec![0., 2., 1.];
-        assert_eq!(normalize(vec), vec![0., 1., 0.5]);
+        assert_eq!(Scene::normalize(vec), vec![0., 1., 0.5]);
     }
 
-    //#[cfg_attr(test, automock)]
-    //impl Scene {
+    use crate::scene::{camera::Camera, light::Light, object::{material::Material, sphere::Sphere}};
 
-
-    use crate::scene::{camera::Camera, light::Light};
-
-    fn create_scene() -> Scene{
+    fn create_empty_scene() -> Scene{
         let camera = Camera::new(
             Vec3d{x:0., y:0., z:0.},
             Vec3d{x:1., y:0., z:0.},
@@ -304,11 +297,10 @@ mod tests {
             objects: vec![],
         }
     }
-    //}
 
     #[test]
     fn test_monothread(){
-        let scene = create_scene();
+        let scene = create_empty_scene();
         let colors = scene.render_monothread();
         assert_eq!(colors[0], 1.);
         assert_eq!(colors[1], 0.);
@@ -317,7 +309,7 @@ mod tests {
 
     #[test]
     fn test_multithread(){
-        let scene = create_scene();
+        let scene = create_empty_scene();
         let colors = scene.render_multithread();
         assert_eq!(colors[0], 1.);
         assert_eq!(colors[1], 0.);
@@ -326,10 +318,42 @@ mod tests {
 
     #[test]
     fn test_rayon(){
-        let scene = create_scene();
+        let scene = create_empty_scene();
         let colors = scene.render_rayon();
         assert_eq!(colors[0], 1.);
         assert_eq!(colors[1], 0.);
         assert_eq!(colors[2], 0.);
+    }
+
+    #[test]
+    fn test_diffuse(){
+        let mut scene = create_empty_scene();
+        scene.lights.push(
+            Light{
+                color: Color { r: 0., g: 1., b: 1. },
+                intensity: 1.,
+                light_type: LightType::PointLight { pos: Vec3d { x: -1., y: 0., z: 0. } }
+            }
+        );
+        let mut sphere_material = Material::default();
+        sphere_material.diffuse.g = 1.;
+        sphere_material.reflectivity = 0.;
+        scene.objects.push(
+            Object::Sphere(Sphere{
+                center: Vec3d { x: 1., y: 0., z: 0. },
+                material: sphere_material.clone(),
+                radius: 1.,
+            })
+        );
+        let intersect = Intersect{
+            dist: 1.,
+            pos: Vec3d { x: 0., y: 0., z: 0. },
+            normal: Vec3d { x: 1., y: 0., z: 0. },
+            material: sphere_material.clone(),
+        };
+        let color = scene.compute_diffuse(&intersect);
+        assert_eq!(color.r, 0.);
+        assert_eq!(color.g, 0.);
+        assert_eq!(color.b, 0.);
     }
 }
